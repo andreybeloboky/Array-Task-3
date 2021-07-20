643389871aba21a534c018e0fd3b9eed13310161
Andrey Beloboky <andrei.beloboky@yandex.ru>
2021-07-20 19:06:50 +0300

    rename setOfOrderedNumbers - arrayOfOrderedNumbers, Add braces in BinarySearch.java (if)

--- src/com/company/BinarySearch.java
+++ src/com/company/BinarySearch.java
@@ -6,17 +6,19 @@ package com.company;
  * @return - found number in array ;
  */
 public class BinarySearch {
-    public static int findingANumberFromAnArray(int[] setOfOrderedNumbers, int key) {
+    public static int findingANumberFromAnArray(int[] arrayOfOrderedNumbers, int key) {
         int low = 0;
-        int high = setOfOrderedNumbers.length;
+        int high = arrayOfOrderedNumbers.length;
         while (low <= high) {
             int middle = low + (high - low) / 2;
-            if (key < setOfOrderedNumbers[middle]) {
+            if (key < arrayOfOrderedNumbers[middle]) {
                 high = middle - 1;
-            } else if (key > setOfOrderedNumbers[middle]) {
-                low = middle + 1;
             } else {
-                return middle;
+                if (key > arrayOfOrderedNumbers[middle]) {
+                    low = middle + 1;
+                } else {
+                    return middle;
+                }
             }
         }
         return -1;
